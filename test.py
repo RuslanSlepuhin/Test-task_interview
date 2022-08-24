ea1a69d4f9ade38b924b6c2de976f93a77d672e1
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2022-08-24 11:26:00 +0300

    'test'

---
+++ test.py
@@ -0,0 +1,42 @@
+
+def sort_words(file_name):
+    """
+    В исходном файле находятся строки с переводом английских слов на русские в формате
+    Astur-Leonese ; Asturian ; Asturian-Leonese ; Astur	астурийский ; астурлеонский
+    Это значит, что каждое из английских слов (Astur-Leonese ; Asturian ; Asturian-Leonese ; Astur) может переводиться
+    как любое русское (астурийский ; астурлеонский)
+    Также встречаются простые варианты где в строке 1 английское и русское слово
+    Abidjan	Абиджан
+    Английские слова отделены от русских переводов символом табуляции
+    Функция парсит этот файл и на базе него делает 2 файла - English.txt и Russian txt, где каждой строке на английском
+    будет соответствовать строка перевода на русском в другом файле во всех возможных вариантах.
+    Пример текста, который должен получиться в файлах, после работы вашего кода:
+
+    English.txt				Russian.txt
+
+    Astur-Leonese				астурийский
+    Astur-Leonese				астурлеонский
+    Asturian				    астурийский
+    Asturian				    астурлеонский
+    Asturian-Leonese			астурийский
+    Asturian-Leonese			астурлеонский
+    ....						....
+    :param file_name: передается имя файла (вместе с path), который нужно распарсить
+    :return: None
+    """
+
+    with open(file_name, encoding='utf-8', mode='r') as file:
+        for line in file:  # читает построчно
+            line_split = line.split(f'\t')  # отделяет английские слова от русских по символу табуляции
+
+            for eng_word in line_split[0].split(';'):
+                for rus_word in line_split[1].split(';'):
+
+                    with open('English.txt', encoding='utf-8', mode='a') as file_eng:
+                        file_eng.writelines(f'{eng_word.strip()}\n')
+
+                    with open('Russian.txt', encoding='utf-8', mode='a') as file_rus:
+                        file_rus.writelines(f'{rus_word.strip()}\n')
+
+
+sort_words(file_name='PythonTest.txt')
